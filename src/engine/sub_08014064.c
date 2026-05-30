3da00153a945aa12464aca7412a9bb44fffe302e
JRickey <jack.rickey99@gmail.com>
2026-05-30 02:20:43 -0400

    Decompile sub_08014064
    Simple void wrapper: calls sub_08010A44(5, 3) then sub_08017000().
    Also peeled callee sub_08017000 (previously a raw text blob).

--- src/engine/sub_08014064.c
+++ src/engine/sub_08014064.c
@@ -1,2 +1,11 @@
 #include "macros.h"
 #include "types.h"
+
+extern void sub_08010A44(u8 a, u8 b);
+extern void sub_08017000(void);
+
+void sub_08014064(void)
+{
+    sub_08010A44(5, 3);
+    sub_08017000();
+}
