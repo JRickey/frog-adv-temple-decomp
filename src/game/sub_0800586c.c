6b2b3490dc247d2f54532724607c3afbc820340b
JRickey <jack.rickey99@gmail.com>
2026-05-30 02:04:17 -0400

    Decompile sub_0800586C
    Sets gGameStuff.pendingMode = 16, calls sub_0800AF50 with the caller's
    r0 arg passed through, then zeroes gIwram_6110._field_33. Straight-line
    3-op void init; matched first try with pure C.

--- src/game/sub_0800586c.c
+++ src/game/sub_0800586c.c
@@ -2,3 +2,12 @@
 #include "iwram.h"
 #include "macros.h"
 #include "types.h"
+
+extern void sub_0800AF50(u32 arg);
+
+void sub_0800586C(u32 arg)
+{
+    gGameStuff.pendingMode = 16;
+    sub_0800AF50(arg);
+    gIwram_6110._field_33 = 0;
+}
