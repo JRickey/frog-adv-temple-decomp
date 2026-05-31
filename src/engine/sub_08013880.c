4272e56f223b96343f64231dead087527606296e
JRickey <jack.rickey99@gmail.com>
2026-05-31 02:17:39 -0400

    Decompile sub_08013880
    Three-call init wrapper: calls sub_0801377C() then sub_0800EBDC(2) and
    sub_0800EE34(2). Straight-line, no locals, pure C first-try match.

--- src/engine/sub_08013880.c
+++ src/engine/sub_08013880.c
@@ -1,2 +1,13 @@
 #include "macros.h"
 #include "types.h"
+
+extern void sub_0801377C(void);
+extern void sub_0800EBDC(u8 arg);
+extern void sub_0800EE34(u8 layer);
+
+void sub_08013880(void)
+{
+    sub_0801377C();
+    sub_0800EBDC(2);
+    sub_0800EE34(2);
+}
