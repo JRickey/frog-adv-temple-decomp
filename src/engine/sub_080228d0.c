a7956134abdbab4950705c5ae793abe620f05355
JRickey <jack.rickey99@gmail.com>
2026-05-31 01:10:52 -0400

    Decompile sub_080228D0
    Single call to sub_08021140 with 7 args (3 on stack): loads sLevelLayout_310C44
    as the data pointer, passes 0x47/0x10/0x10 in registers and 0x195/0xb/0 on stack.

--- src/engine/sub_080228d0.c
+++ src/engine/sub_080228d0.c
@@ -1,2 +1,10 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021140(s32 a0, const void *a1, s32 a2, s32 a3, s32 a4, s32 a5, s32 a6);
+extern const u32 sLevelLayout_310C44[12];
+
+void sub_080228D0(void)
+{
+    sub_08021140(0x47, sLevelLayout_310C44, 0x10, 0x10, 0x195, 0xb, 0);
+}
