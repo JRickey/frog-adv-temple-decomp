6ba79f9af995a3a77111ddb41210df68e30c714e
JRickey <jack.rickey99@gmail.com>
2026-05-31 14:25:31 -0400

    Decompile sub_08012E00
    Simple init sequence: calls sub_0800E85C(3), sub_0800EE34(2),
    sub_0800EB1C(), sub_08012D40(), sub_0800F24C(3), sub_0800EBDC(3),
    sub_08016A40() in order. Straight-line with no locals needed.

--- src/engine/sub_08012d88.c
+++ src/engine/sub_08012d88.c
@@ -34,3 +34,22 @@ void sub_08012D88(void)
     sub_08010A44(4, 4);
     sub_08017000();
 }
+
+extern void sub_0800E85C(u8 arg);
+extern void sub_0800EE34(u8 layer);
+extern void sub_0800EB1C(void);
+extern void sub_08012D40(void);
+extern void sub_0800F24C(u8 arg);
+extern void sub_0800EBDC(u8 arg);
+extern void sub_08016A40(void);
+
+void sub_08012E00(void)
+{
+    sub_0800E85C(3);
+    sub_0800EE34(2);
+    sub_0800EB1C();
+    sub_08012D40();
+    sub_0800F24C(3);
+    sub_0800EBDC(3);
+    sub_08016A40();
+}
