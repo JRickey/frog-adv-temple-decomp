5458e050211ea0651293e8360c004faf9e7d00f9
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:50:52 -0400

    Decompile sub_08015A2C

--- src/engine/sub_08015a00.c
+++ src/engine/sub_08015a00.c
@@ -14,6 +14,10 @@ struct TilemapTableEntry {
 };
 
 extern void sub_08012BC4(u8 mode, u16 a, u16 b, u16 c, u16 d, void *tiles, u8 e);
+extern void sub_0800E7D4(void);
+extern void sub_0802BC94(void);
+extern void sub_08017000(void);
+extern void sub_0800534C(void);
 
 #define sTilemapTable_307CE8 (*(const struct TilemapTableEntry *)0x08307ce8)
 
@@ -22,4 +26,13 @@ void sub_08015A00(void)
     const struct TilemapTableEntry *desc = &sTilemapTable_307CE8;
 
     sub_08012BC4(desc->unk08, desc->unk00, desc->unk02, desc->unk04, desc->unk06, desc->tiles, 1);
+}
+
+void sub_08015A2C(void)
+{
+    sub_0800E7D4();
+    if (gIwram_6110.spawnMask == 0)
+        sub_0802BC94();
+    sub_08017000();
+    sub_0800534C();
 }
