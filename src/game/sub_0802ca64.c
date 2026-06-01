b449f9eabaa0f14373bbd7c33f923952877856e7
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:59:00 -0400

    Decompile sub_0802CA70

--- src/game/sub_0802ca64.c
+++ src/game/sub_0802ca64.c
@@ -3,8 +3,15 @@
 #include "types.h"
 
 extern void sub_0802C200(u32 a0);
+extern void text_0802bdbc(void);
 
 void sub_0802CA64(void)
 {
     sub_0802C200(0x58);
+}
+
+void sub_0802CA70(void)
+{
+    text_0802bdbc();
+    sub_0802C200(0x5B);
 }
