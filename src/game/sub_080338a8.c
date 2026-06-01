ed7e4bd678323c308992a22f6ec23423660e4778
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:54:47 -0400

    Decompile sub_080338A8

--- src/game/sub_080338a8.c
+++ src/game/sub_080338a8.c
@@ -1,1 +1,23 @@
 #include "types.h"
+
+#define gUnknown_030065E4 (*(const void **)0x030065e4)
+#define sUnknown_082F8E98 ((const void *)0x082f8e98)
+#define sUnknown_082F8EA4 ((const void *)0x082f8ea4)
+
+u32 sub_080338A8(u32 value)
+{
+    u32 ret;
+
+    value = (u16)value;
+    ret = 0;
+    if (value == 4) {
+        gUnknown_030065E4 = sUnknown_082F8E98;
+    } else if (value == 0x40) {
+        gUnknown_030065E4 = sUnknown_082F8EA4;
+    } else {
+        gUnknown_030065E4 = sUnknown_082F8E98;
+        ret = 1;
+    }
+
+    return ret;
+}
