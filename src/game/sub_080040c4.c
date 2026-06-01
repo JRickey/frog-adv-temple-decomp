e35f7acbdc821434276911e0c8eecebaa8020a1f
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:46:38 -0400

    Decompile sub_080040C4

--- src/game/sub_080040c4.c
+++ src/game/sub_080040c4.c
@@ -1,1 +1,17 @@
 #include "game.h"
+
+extern void sub_0800A2D8(void);
+extern void sub_080008DC(void);
+extern void sub_0800A328(void);
+extern void sub_080094F8(void);
+extern void sub_08009984(void);
+
+void sub_080040C4(void)
+{
+    sub_0800A2D8();
+    sub_080008DC();
+    sub_0800A328();
+    sub_080094F8();
+    sub_08009984();
+    gGameStuff._unk14++;
+}
