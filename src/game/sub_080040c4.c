1babbb88690a10776ff6627981dfa2f8f2aaed2b
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:47:27 -0400

    Decompile sub_08004104

--- src/game/sub_080040c4.c
+++ src/game/sub_080040c4.c
@@ -1,10 +1,13 @@
 #include "game.h"
+#include "iwram.h"
 
 extern void sub_0800A2D8(void);
 extern void sub_080008DC(void);
 extern void sub_0800A328(void);
 extern void sub_080094F8(void);
 extern void sub_08009984(void);
+extern void sub_08013FA0(void);
+extern void sub_0800DE80(void);
 extern u32 sub_08009C14(u8 *state);
 
 void sub_080040C4(void)
@@ -21,4 +24,11 @@ void sub_080040EC(u8 *state)
 {
     if (sub_08009C14(state) == 0)
         *state = 8;
+}
+
+void sub_08004104(void)
+{
+    gIwram_3720._field_17 = 2;
+    sub_08013FA0();
+    sub_0800DE80();
 }
