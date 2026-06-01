e79a5b938c58e0f4c8e1fbcd2e88db535bbc2f0d
JRickey <jack.rickey99@gmail.com>
2026-06-01 15:46:56 -0400

    Decompile sub_080040EC

--- src/game/sub_080040c4.c
+++ src/game/sub_080040c4.c
@@ -5,6 +5,7 @@ extern void sub_080008DC(void);
 extern void sub_0800A328(void);
 extern void sub_080094F8(void);
 extern void sub_08009984(void);
+extern u32 sub_08009C14(u8 *state);
 
 void sub_080040C4(void)
 {
@@ -14,4 +15,10 @@ void sub_080040C4(void)
     sub_080094F8();
     sub_08009984();
     gGameStuff._unk14++;
+}
+
+void sub_080040EC(u8 *state)
+{
+    if (sub_08009C14(state) == 0)
+        *state = 8;
 }
