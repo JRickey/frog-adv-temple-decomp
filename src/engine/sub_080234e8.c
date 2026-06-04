1f73e5e6de15d31dd93a1463f451d40895375403
JRickey <jack.rickey99@gmail.com>
2026-06-04 01:03:21 -0400

    Decompile sub_080234E8
    Simple call-through to sub_08021140 with 7 args (r0=0x5f,
    sLevelLayout_311488, 0, 0x4c, 0x231, 14, 0). First-try match.

--- src/engine/sub_080234e8.c
+++ src/engine/sub_080234e8.c
@@ -1,2 +1,10 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021140(s32 a0, const void *a1, s32 a2, s32 a3, s32 a4, s32 a5, s32 a6);
+extern const u32 sLevelLayout_311488[4];
+
+void sub_080234E8(void)
+{
+    sub_08021140(0x5f, sLevelLayout_311488, 0, 0x4c, 0x231, 14, 0);
+}
