5ea42d47d1d67263da6370b299ebd6694383fc91
JRickey <jack.rickey99@gmail.com>
2026-06-05 09:57:37 -0400

    Decompile sub_08024234
    Single call to sub_08021140 with 7 args (4-6 stack-passed). Adds
    sLevelLayout_312210 linker alias (sLevelLayoutData_311F80 + 0x290).

--- src/engine/sub_08024234.c
+++ src/engine/sub_08024234.c
@@ -1,2 +1,11 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021140(u32 a0, const void *a1, u32 a2, u32 a3, u32 a4, u32 a5, u32 a6);
+
+extern const u32 sLevelLayout_312210[];
+
+void sub_08024234(void)
+{
+    sub_08021140(0x5f, sLevelLayout_312210, 16, 25, 0x141, 7, 0);
+}
