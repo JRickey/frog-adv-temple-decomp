b094145848cc75fc7cf27e6ec1af79f5621d7796
JRickey <jack.rickey99@gmail.com>
2026-06-05 09:53:39 -0400

    Decompile sub_08015B6C

--- asm/disasm_0x08015b6c.s
+++
@@ -1,7 +0,0 @@
-        .include "asm/macros.inc"
-        .syntax unified
-
-        thumb_func_start sub_08015B6C
-sub_08015B6C: @ 0x08015b6c
-        .incbin "frog_us_baserom.gba", 0x15b6c, 0xb8
-        thumb_func_end sub_08015B6C
---
+++ src/engine/sub_08015b6c.c
@@ -0,0 +1,91 @@
+#include "types.h"
+
+struct BlitState_15B6C {
+    u8 _pad00[26];
+    u16 stride;
+};
+
+struct BlitSource_15B6C {
+    u8 _pad00[12];
+    const u16 *tiles;
+    u8 _pad10[32];
+    u16 srcStride;
+};
+
+void sub_08015B6C(u8 mode, s16 dstX, s16 dstY, u8 widthArg, u8 heightArg, struct BlitSource_15B6C *src)
+{
+    register struct BlitSource_15B6C *srcp asm("r8");
+    register u16 *dst asm("r6");
+    register const u16 *srcData asm("r3");
+    register u32 row asm("r0");
+    register u32 r1slot asm("r1");
+    register u32 width asm("r5");
+    register u32 height asm("r4");
+    register u32 dyh asm("ip");
+    register struct BlitState_15B6C *loopState asm("r9");
+    u8 col;
+    register u16 dstStride asm("r3");
+    u32 scratch2;
+    register s32 dx asm("r2");
+
+    srcp = src;
+    mode = (u8)(mode + ((u32)srcp - (u32)srcp));
+    r1slot = (u16)(dstX + ((u32)srcp - (u32)srcp));
+    dyh = (u16)dstY;
+    width = (u8)(widthArg + ((u32)srcp - (u32)srcp));
+    height = (u8)(heightArg + ((u32)srcp - (u32)srcp));
+
+    switch (mode) {
+    case 0:
+        dst = (u16 *)(0x80 << 18);
+        break;
+    case 1:
+        dst = (u16 *)0x02010000;
+        break;
+    case 2:
+        dst = (u16 *)0x02020000;
+        break;
+    }
+
+    row = r1slot << 16;
+    dx = (s32)row >> 16;
+    {
+        register u32 earlyScratch asm("r7");
+
+        earlyScratch = dyh;
+        row = earlyScratch << 16;
+        row = (s32)row >> 16;
+    }
+    r1slot = 0x030060A0;
+    dstStride = ((struct BlitState_15B6C *)r1slot)->stride;
+    row *= dstStride;
+    row = dx + row;
+    dst += row;
+
+    scratch2 = (u32)srcp;
+    srcData = *(const u16 **)(scratch2 + 12);
+    row = 0;
+    loopState = (struct BlitState_15B6C *)r1slot;
+    if (row < height) {
+        register u32 scratch asm("r2");
+
+        do {
+            col = 0;
+            r1slot = row + 1 + scratch2 - scratch2;
+            if (col < width) {
+                do {
+                    *dst++ = *srcData++;
+                    col = (u8)(col + 1);
+                } while (col < width);
+            }
+
+            scratch = (u32)loopState;
+            scratch = ((struct BlitState_15B6C *)scratch)->stride;
+            dst += scratch - width + scratch2 - scratch2;
+            scratch2 = (u32)srcp;
+            scratch2 = ((struct BlitSource_15B6C *)scratch2)->srcStride;
+            srcData += scratch2 - width;
+            row = (u8)r1slot;
+        } while (row < height);
+    }
+}
