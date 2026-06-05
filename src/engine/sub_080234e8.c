8e8a2ee4bd02a2613c4e7cb8c42ec1db0df90836
JRickey <jack.rickey99@gmail.com>
2026-06-05 03:45:32 -0400

    Decompile sub_08023514
    Calls sub_08021510(0x5f, sLevelLayout_311488, 0x4c, &gIwram_6110, 0).
    Added to src/engine/sub_080234e8.c alongside the sibling sub_080234E8.

--- src/engine/sub_080234e8.c
+++ src/engine/sub_080234e8.c
@@ -2,9 +2,15 @@
 #include "types.h"
 
 extern void sub_08021140(s32 a0, const void *a1, s32 a2, s32 a3, s32 a4, s32 a5, s32 a6);
+extern void sub_08021510(u8 a0, const void *a1, u8 a2, void *a3, u8 a4);
 extern const u32 sLevelLayout_311488[4];
 
 void sub_080234E8(void)
 {
     sub_08021140(0x5f, sLevelLayout_311488, 0, 0x4c, 0x231, 14, 0);
+}
+
+void sub_08023514(void)
+{
+    sub_08021510(0x5f, sLevelLayout_311488, 0x4c, &gIwram_6110, 0);
 }
