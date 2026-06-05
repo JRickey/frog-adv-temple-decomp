2adef2a4bea48c029062c5ec15d0bc7a70e57358
JRickey <jack.rickey99@gmail.com>
2026-06-05 09:53:15 -0400

    Decompile sub_08024070
    Simple wrapper: calls sub_08021510(20, sLevelLayout_312148, 2,
    &gIwram_6110, 0). Adds linker-assigned symbol sLevelLayout_312148
    (= sLevelLayoutData_311F80 + 0x1c8) for the sub-table at 0x08312148.

--- src/engine/sub_08024070.c
+++ src/engine/sub_08024070.c
@@ -1,2 +1,10 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021510(u8 a0, const void *a1, u8 a2, void *a3, u8 a4);
+extern const u32 sLevelLayout_312148[];
+
+void sub_08024070(void)
+{
+    sub_08021510(20, sLevelLayout_312148, 2, &gIwram_6110, 0);
+}
