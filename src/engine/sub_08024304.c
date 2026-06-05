b7550991da397f31ba3af38eb1ef4740518d2c01
JRickey <jack.rickey99@gmail.com>
2026-06-05 10:17:15 -0400

    Decompile sub_08024304
    Simple leaf: calls sub_08021510(0x65, sLevelLayout_312270, 0x0b, &gIwram_6110, 0).
    Added linker-assigned symbol sLevelLayout_312270 = sLevelLayoutData_311F80 + 0x2f0.

--- src/engine/sub_08024304.c
+++ src/engine/sub_08024304.c
@@ -1,2 +1,11 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021510(u8 a0, const void *a1, u8 a2, void *a3, u8 a4);
+
+extern const u32 sLevelLayout_312270[];
+
+void sub_08024304(void)
+{
+    sub_08021510(0x65, sLevelLayout_312270, 0x0b, &gIwram_6110, 0);
+}
