4f94935d9826398c75ea84d8b1554cf6c995d5b9
JRickey <jack.rickey99@gmail.com>
2026-06-05 10:05:56 -0400

    Decompile sub_08024180
    Loop i=0..5 calling sub_080210A0(i + 0x59, sLevelLayoutPtrs_312E6C[i],
    16, 13, (i & 3) << 4 | 0x101, 6, 3, 3). Follows the same do/while +
    u8 counter pattern as sub_08023FAC and the surrounding cluster.

--- src/engine/sub_08024180.c
+++ src/engine/sub_08024180.c
@@ -1,2 +1,17 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_080210A0(u16 a0, const void *a1, u16 a2, u8 a3, u16 a4, u8 a5, u8 a6, u8 a7);
+
+extern const u32 sLevelLayoutPtrs_312E6C[6];
+
+void sub_08024180(void)
+{
+    u8 i;
+
+    i = 0;
+    do {
+        sub_080210A0(i + 0x59, (const void *)sLevelLayoutPtrs_312E6C[i], 16, 13, ((i & 3) << 4) | 0x101, 6, 3, 3);
+        i++;
+    } while (i <= 5);
+}
