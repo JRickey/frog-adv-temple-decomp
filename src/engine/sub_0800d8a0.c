dd4d67e264be661be3c213ce63066a953c40fef2
JRickey <jack.rickey99@gmail.com>
2026-06-05 15:24:43 -0400

    Reduce active pins in sub_0800d8a0

--- src/engine/sub_0800d8a0.c
+++ src/engine/sub_0800d8a0.c
@@ -15,10 +15,9 @@ extern u32 sub_0800DA70(void);
 
 void sub_0800D8A0(void)
 {
-    register u8 *base6110 asm("r4");
+    register u8 *base asm("r4");
     register u8 *saved6110 asm("r5");
     u8 *new_var;
-    register u8 *base3720 asm("r4");
     register u8 *entry asm("r6");
     register u32 mask asm("r3");
     u32 c;
@@ -26,16 +25,16 @@ void sub_0800D8A0(void)
     u8 *new_var2;
     u8 *ep;
 
-    base6110 = (u8 *)&gIwram_6110;
-    c = sub_0800679C(base6110, 3, 0) << 24;
-    saved6110 = base6110;
+    base = (u8 *)&gIwram_6110;
+    c = sub_0800679C(base, 3, 0) << 24;
+    saved6110 = base;
     if (c != 0) {
         new_var2 = saved6110;
         i = 0;
-        base3720 = (u8 *)gEntities;
-        asm("" ::"r"(base3720));
+        base = (u8 *)gEntities;
+        asm("" ::"r"(base));
         mask = 8;
-        new_var = base3720;
+        new_var = base;
         do {
             ep = (u8 *)((u32)((s8)i + 3) * 56 + (u32)new_var);
             *(u16 *)(ep + 0x34) = *(u16 *)(ep + 0x34) | mask;
@@ -69,7 +68,7 @@ u32 *sub_0800D924(u32 *out, u32 mask, u32 limit)
     register u32 found asm("sl");
     register u8 ulimit asm("r8");
     u32 scratch;
-    register s32 slimit asm("ip");
+    s32 slimit;
     s32 sf;
     s32 i;
     u32 loopValue;
@@ -139,22 +138,20 @@ u32 sub_0800D9C8(void)
     register struct IwramAt35E0 *src asm("r4");
     register u32 offset asm("r5");
     register u32 offset2 asm("r3");
-    register u16 *ptr asm("r1");
-    register u32 x asm("r3");
-    register u32 y asm("r1");
+    register u32 addrOrValue asm("r1");
 
     sub_080113E8();
     dst = gEntities;
     src = &gIwram_35E0;
-    x = (u16)src->_field_8;
+    offset2 = (u16)src->_field_8;
     offset = 0x692;
     asm("" : "+r"(offset));
-    ptr = (u16 *)((u8 *)dst + offset);
-    *ptr = x;
-    y = (u16)src->_field_A;
+    addrOrValue = (u32)dst + offset;
+    *(u16 *)addrOrValue = offset2;
+    addrOrValue = (u16)src->_field_A;
     offset2 = 0x694;
     asm("" : "+r"(offset2));
     dst = (struct Entity *)((u8 *)dst + offset2);
-    *(u16 *)dst = y;
+    *(u16 *)dst = addrOrValue;
     return sub_0800DA70();
 }
