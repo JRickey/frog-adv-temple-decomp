96bf27c33898c7bdfc3f3cc8ad6081e62b078c81
JRickey <jack.rickey99@gmail.com>
2026-06-05 20:56:32 -0400

    Decompile sub_08024444
    Single-call wrapper: sub_08021140(0x77, sLevelLayout_312138, 0, 0x4c, 0x351, 14, 0).
    Added sLevelLayout_312138 linker symbol (sLevelLayoutData_311F80 + 0x1b8).

--- src/engine/sub_08024444.c
+++ src/engine/sub_08024444.c
@@ -1,2 +1,11 @@
 #include "iwram.h"
 #include "types.h"
+
+extern void sub_08021140(s32 a0, const void *a1, s32 a2, s32 a3, s32 a4, s32 a5, s32 a6);
+
+extern const u32 sLevelLayout_312138[];
+
+void sub_08024444(void)
+{
+    sub_08021140(0x77, sLevelLayout_312138, 0, 0x4c, 0x351, 14, 0);
+}
