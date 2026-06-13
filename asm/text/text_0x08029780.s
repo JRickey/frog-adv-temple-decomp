5144e4176a5ff8a4ad047dfa5c8032a206c7eab7
JRickey <jack.rickey99@gmail.com>
2026-06-13 10:28:25 -0400

    peel: sub_08029748 [0x08029748, 0x08029780)

--- asm/text/text_0x08029780.s
+++ asm/text/text_0x08029780.s
@@ -1,7 +1,7 @@
 @ Auto-emitted by wire.py
 @ Address-aligned bucket of unparsed baserom bytes.
-@ Range:  [0x08029748, 0x0802a2f0)  (shrunk for sub_08029710 peel)
+@ Range:  [0x08029780, 0x0802a2f0)  (shrunk for sub_08029748 peel)
 
         .section .text, "ax", %progbits
         .balign 1
-        .incbin "frog_us_baserom.gba", 0x0029748, 0x00000ba8
+        .incbin "frog_us_baserom.gba", 0x0029780, 0x00000b70
