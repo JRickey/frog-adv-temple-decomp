605a04c14c14133fb82e3aa3ce3ceae11507ca9b
JRickey <jack.rickey99@gmail.com>
2026-06-13 10:05:13 -0400

    peel: sub_080295AC [0x080295ac, 0x080295d4)

---
+++ asm/text/text_0x080295d4.s
@@ -0,0 +1,7 @@
+@ Auto-emitted by wire.py
+@ Address-aligned bucket of unparsed baserom bytes.
+@ Range:  [0x080295d4, 0x0802a2f0)  (shrunk for sub_080295AC peel)
+
+        .section .text, "ax", %progbits
+        .balign 1
+        .incbin "frog_us_baserom.gba", 0x000295d4, 0x00000d1c
